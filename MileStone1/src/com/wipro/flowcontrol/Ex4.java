d64a5cc2c45f3685363de390698515a5c1644bc5
rohit <rohit@RohitSasanala>
2020-06-24 18:13:26 +0530

    Alphebetic order

---
+++ MileStone1/src/com/wipro/flowcontrol/Ex4.java
@@ -0,0 +1,16 @@
+package com.wipro.flowcontrol;
+
+public class Ex4 {
+	public static void main(String[] args) {
+		char item1='s';
+		char item2='e';
+		if(item1<item2)
+			System.out.println(item1+","+item2);
+		else
+		{
+			System.out.println(item2+","+item1);
+		}
+	}
+	
+
+}
