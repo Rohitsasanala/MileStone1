944fed4ca5e165646ca022a7ecb54c108f294864
rohit <rohit@RohitSasanala>
2020-06-24 19:00:43 +0530

    Sum and average

--- MileStone1/src/com/wipro/arrays/Ex1.java
+++ MileStone1/src/com/wipro/arrays/Ex1.java
@@ -1,7 +1,16 @@
 package com.wipro.arrays;
 
 public class Ex1 {
-public static void main(String[] args) {
-	System.out.println("rohit");
-}
+	public static void main(String[] args) {
+		int[ ] a= new int[]{3,7,9,4,1};
+		int i,s=0;
+		 for (i=0;i<a.length;i++)
+		 {
+			 s = s+a[i];
+		 }
+		int Avg =s/a.length;
+		System.out.println("sum of array "+s+" and  average of array "+Avg);
+
+	}
+
 }
