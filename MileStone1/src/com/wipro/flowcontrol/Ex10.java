f835eecad0766147e4c561ac5cc2c826b2c20af4
rohit <rohit@RohitSasanala>
2020-06-24 18:36:54 +0530

    1 to 10 single line

---
+++ MileStone1/src/com/wipro/flowcontrol/Ex10.java
@@ -0,0 +1,11 @@
+package com.wipro.flowcontrol;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		System.out.print("1");
+		for (int i = 2; i <=10; i++) 
+			System.out.print("	"+i);
+
+	}
+
+}
