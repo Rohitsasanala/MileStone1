1afd9db96cf4bef020725bd88ef792a549d52777
rohit <rohit@RohitSasanala>
2020-06-24 19:05:55 +0530

    Integer to Asci Value

---
+++ MileStone1/src/com/wipro/arrays/Ex4.java
@@ -0,0 +1,12 @@
+package com.wipro.arrays;
+
+public class Ex4 {
+	public static void main(String[] args) {
+		char a[] ={65,97,72,75,77};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+
+}
