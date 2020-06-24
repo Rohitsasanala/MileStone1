2948bf98b11818fc2e5a2cd8416da26ec29b0bcd
rohit <rohit@RohitSasanala>
2020-06-24 18:17:47 +0530

    Gender and Age

---
+++ MileStone1/src/com/wipro/flowcontrol/Ex6.java
@@ -0,0 +1,35 @@
+package com.wipro.flowcontrol;
+
+public class Ex6 {
+	public static void main(String[] args)
+	{
+		String gender=args[0];
+		int age=Integer.parseInt(args[1]);
+		if(gender.equalsIgnoreCase("Female"))
+		{
+		    if(age>=1 && age<=58)
+		    {
+		        System.out.println("Interest=8.2%");
+		    }
+		    else if(age>=59 && age<=120)
+		    {
+		        System.out.println("Interest=7.6%");
+		    }
+		}
+		else if(gender.equalsIgnoreCase("Male"))
+		{
+		     if(age>=1 && age<=60)
+		    {
+		        System.out.println("Interest=9.2%");
+		    }
+		    else if(age>=61 && age<=120)
+		    {
+		        System.out.println("Interest=8.3%");
+		    }
+		    
+		    
+		}
+	}
+	
+
+}
