8e624ff3c15b0eef5253d9325eb7aa2646620d32
rohit <rohit@RohitSasanala>
2020-06-24 17:29:26 +0530

    first commit

--- MileStone1/src/com/wipro/flowcontrol/Ex1.java
+++ MileStone1/src/com/wipro/flowcontrol/Ex1.java
@@ -1,5 +1,29 @@
 package com.wipro.flowcontrol;
 
 public class Ex1 {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		if(a > 0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a < 0)
+		{
+			System.out.println("Negative");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}
+		if(~(a%10 - 1) == b%10)
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+	}
 
 }
